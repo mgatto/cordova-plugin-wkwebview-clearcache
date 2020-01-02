822fcb9417919e29e5d69fcb74927afeb5781aae
Michael Gatto <mgatto@vinsuite.com>
2020-01-02 14:06:22 -0700

    feat: refactor to restrict deletions to a specific domain

--- src/ios/WkCacheClear.m
+++ src/ios/WkCacheClear.m
@@ -1,107 +1,109 @@
 #import "WkCacheClear.h"
 #import <WebKit/WKWebsiteDataStore.h>
 
+
 @implementation WkCacheClear
 @synthesize command;
 
+/* 
+Calls are async operations and SHOULD BE CALLED FROM THE MAIN THREAD (?)
+*/
 
 - (void)task:(CDVInvokedUrlCommand *)command
 {
     NSLog(@"Cordova iOS WkCacheClear() called");
-
+    
     @try {
         self.command = command;
-
-        // if (floor(NSFoundationVersionNumber) >= NSFoundationVersionNumber_iOS_9_0) {
+        
         if (floor(NSFoundationVersionNumber) < NSFoundationVersionNumber_iOS_9_0) {
             NSLog(@"iOS version is too old: %f", NSFoundationVersionNumber);
             @throw [[NSException alloc] initWithName:@"iOSVersionTooOld" reason:[NSString stringWithFormat:@"iOS version is too old: %f", NSFoundationVersionNumber] userInfo:nil];
         }
-        /*else {
-
-        }*/
-
+        
         NSArray* cachesToDelete;
-        NSArray* arguments = command.arguments;
-        NSDictionary* options = [arguments objectAtIndex:0];
-
-        if ( ![options isKindOfClass:[NSNull class]] ) {
-            cachesToDelete = [options objectForKey:@"delete"];
+        NSDictionary* options;
+        
+        if ([command.arguments count] == 1) {
+            options = [command.arguments objectAtIndex:0];
+            
+            if ( [options isKindOfClass:[NSDictionary class]] ) {
+                cachesToDelete = [options objectForKey:@"delete"];
+                
+                if ( ![options objectForKey:@"domain"]) {
+                    /* domain is required. */
+                    @throw [[NSException alloc] initWithName:@"BadParameters" reason:[NSString stringWithFormat:@"The option: domain is required"] userInfo:nil];
+                }
+            } else {
+                @throw [[NSException alloc] initWithName:@"BadParameters" reason:[NSString stringWithFormat:@"The options object cannot be empty"] userInfo:nil];
+            }
+        } else {
+            @throw [[NSException alloc] initWithName:@"BadParameters" reason:[NSString stringWithFormat:@"The options object is not present"] userInfo:nil];
         }
-
-        //NSLog(@"delete options = %@", cachesToDelete);
+        
+        NSLog(@"clear cache options = %@", cachesToDelete);
+        
+        /* These are the default types, which are always cleared */
         NSMutableSet *websiteDataTypes = [NSMutableSet setWithArray:@[WKWebsiteDataTypeMemoryCache,WKWebsiteDataTypeOfflineWebApplicationCache]];
-
-        if ([cachesToDelete containsObject:@"cookies"]) {
-            NSLog(@"cookies will be cleared");
-
-            WKWebsiteDataStore *dataStore = [WKWebsiteDataStore defaultDataStore];
-            [dataStore fetchDataRecordsOfTypes:[WKWebsiteDataStore allWebsiteDataTypes]
-                             completionHandler:^(NSArray<WKWebsiteDataRecord *> * _Nonnull records) {
-                                 for (WKWebsiteDataRecord *record  in records) {
-                                    // only delete OUR cookies!!
-                                     if ( [record.displayName containsString:@"ewinerysolutions.com"]) {
-                                         [[WKWebsiteDataStore defaultDataStore] removeDataOfTypes:record.dataTypes
-                                                                                   forDataRecords:@[record]
-                                                                                completionHandler:^{
-                                                                                    NSLog(@"Cookies for %@ deleted successfully", record.displayName);
-                                                                                }];
-                                     }
-                                 }
-                             }];
-        }
-
+        
+        /* Delete the file cache? */
         if ([cachesToDelete containsObject:@"assets"]) {
             [websiteDataTypes addObject:WKWebsiteDataTypeDiskCache];
             NSLog(@"assets will be cleared");
         }
-
-        // @TODO get a passed in date from client, which could be the BuildDate from the BuildInfo or Device plugins?
-        NSDate *dateFrom = [NSDate dateWithTimeIntervalSince1970:0];
-        [[WKWebsiteDataStore defaultDataStore] removeDataOfTypes:websiteDataTypes modifiedSince:dateFrom completionHandler:^{}];
-
+        
+        /* Delete the cookies? */
+        if ([cachesToDelete containsObject:@"cookies"]) {
+            [websiteDataTypes addObject:WKWebsiteDataTypeCookies];
+            NSLog(@"cookies will be cleared");
+        }
+            
+        WKWebsiteDataStore *dataStore = [WKWebsiteDataStore defaultDataStore];
+        [dataStore fetchDataRecordsOfTypes:websiteDataTypes
+                         completionHandler:^(NSArray<WKWebsiteDataRecord *> * _Nonnull records) {
+                             //NSLog(@"Record = %@", records);
+                             
+                             for (WKWebsiteDataRecord *record  in records) {
+                                 /* only delete records for the specified domain */
+                                 if ( [record.displayName containsString:options[@"domain"]]) {
+                                     [[WKWebsiteDataStore defaultDataStore] removeDataOfTypes:record.dataTypes
+                                                                               forDataRecords:@[record]
+                                                                            completionHandler:^{}
+                                     ];
+                                 }
+                             }
+                         }
+        ];
+        
         [self success];
-
-    }@catch (NSException *exception) {
+            
+    }
+    @catch (NSException *exception) {
         NSLog(@"%@", exception);
-        [self error:[NSString stringWithFormat:@"EXCEPTION: %@", exception.reason]];
+        [self error:[NSString stringWithFormat:@"EXCEPTION: %@: %@", exception.name, exception.reason]];
     }
 }
 
 
 - (void)success
 {
-    NSString *resultMsg = @"Cordova iOS wkwebview cache cleared.";
     NSLog(@"%@", resultMsg);
-
-    CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
-                                                messageAsString:[resultMsg stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet] ]];
-
-    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+    
+    CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    
+    [self.commandDelegate sendPluginResult:result callbackId:self.command.callbackId];
 }
-
-
+            
+            
 - (void)error:(NSString *)message
 {
     NSString *resultMsg = [NSString stringWithFormat:@"Error while clearing wkwebview cache (%@).", message];
     NSLog(@"%@", resultMsg);
-
+    
     CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
                                                 messageAsString:[resultMsg stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet] ]];
-
-    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+    
+    [self.commandDelegate sendPluginResult:result callbackId:self.command.callbackId];
 }
-
+            
 @end
-
-
-//WKWebsiteDataTypeDiskCache,
-//WKWebsiteDataTypeOfflineWebApplicationCache,
-//WKWebsiteDataTypeMemoryCache,
-//WKWebsiteDataTypeLocalStorage,
-//WKWebsiteDataTypeCookies,
-//WKWebsiteDataTypeSessionStorage,
-//WKWebsiteDataTypeIndexedDBDatabases,
-//WKWebsiteDataTypeWebSQLDatabases,
-//WKWebsiteDataTypeFetchCache, //(iOS 11.3, *)
-//WKWebsiteDataTypeServiceWorkerRegistrations, //(iOS 11.3, *)
--- www/WkCacheClear.js
+++ www/WkCacheClear.js
@@ -1,10 +1,22 @@
 var exec = require('cordova/exec');
 
-// Usage
-// window.WkCacheClear({delete: ['cookies','assets'], cookieDomain: 'example.com'}, () => '', (error) => '');
-
+/**
+ * Usage:
+ * 
+ * window.WkCacheClear({delete: ['cookies','assets'], 'domain': 'example.com'}, () => '', (error) => '');
+ * 
+ * options object is required.
+ *   domain setting is required
+ *   delete setting (array) is optional. Allowed values are an empty array (but, why?), 'cookies' and/or 'assets'.
+ * 
+ */ 
 var WkCacheClear = function (options, success, error) {
-    exec(success, error, 'WkCacheClear', 'task', [options]);
+    if ( !options.hasOwnProperty('domain')) {
+        error('option: "domain" is required');
+    } 
+    else {
+        exec(success, error, 'WkCacheClear', 'task', [options]);
+    }
 };
 
 module.exports = WkCacheClear;
