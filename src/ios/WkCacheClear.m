410c40c473209a1584c8838f049a864f08d8ffce
julien.fougere <julien.fougere@transatel.net>
2020-08-24 18:05:13 +0800

    Fix compilation issue

--- src/ios/WkCacheClear.m
+++ src/ios/WkCacheClear.m
@@ -5,31 +5,31 @@
 @implementation WkCacheClear
 @synthesize command;
 
-/* 
+/*
 Calls are async operations and SHOULD BE CALLED FROM THE MAIN THREAD (?)
 */
 
 - (void)task:(CDVInvokedUrlCommand *)command
 {
     NSLog(@"Cordova iOS WkCacheClear() called");
-    
+
     @try {
         self.command = command;
-        
+
         if (floor(NSFoundationVersionNumber) < NSFoundationVersionNumber_iOS_9_0) {
             NSLog(@"iOS version is too old: %f", NSFoundationVersionNumber);
             @throw [[NSException alloc] initWithName:@"iOSVersionTooOld" reason:[NSString stringWithFormat:@"iOS version is too old: %f", NSFoundationVersionNumber] userInfo:nil];
         }
-        
+
         NSArray* cachesToDelete;
         NSDictionary* options;
-        
+
         if ([command.arguments count] == 1) {
             options = [command.arguments objectAtIndex:0];
-            
+
             if ( [options isKindOfClass:[NSDictionary class]] ) {
                 cachesToDelete = [options objectForKey:@"delete"];
-                
+
                 if ( ![options objectForKey:@"domain"]) {
                     /* domain is required. */
                     @throw [[NSException alloc] initWithName:@"BadParameters" reason:[NSString stringWithFormat:@"The option: domain is required"] userInfo:nil];
@@ -40,29 +40,29 @@ Calls are async operations and SHOULD BE CALLED FROM THE MAIN THREAD (?)
         } else {
             @throw [[NSException alloc] initWithName:@"BadParameters" reason:[NSString stringWithFormat:@"The options object is not present"] userInfo:nil];
         }
-        
+
         NSLog(@"clear cache options = %@", cachesToDelete);
-        
+
         /* These are the default types, which are always cleared */
         NSMutableSet *websiteDataTypes = [NSMutableSet setWithArray:@[WKWebsiteDataTypeMemoryCache,WKWebsiteDataTypeOfflineWebApplicationCache]];
-        
+
         /* Delete the file cache? */
         if ([cachesToDelete containsObject:@"assets"]) {
             [websiteDataTypes addObject:WKWebsiteDataTypeDiskCache];
             NSLog(@"assets will be cleared");
         }
-        
+
         /* Delete the cookies? */
         if ([cachesToDelete containsObject:@"cookies"]) {
             [websiteDataTypes addObject:WKWebsiteDataTypeCookies];
             NSLog(@"cookies will be cleared");
         }
-            
+
         WKWebsiteDataStore *dataStore = [WKWebsiteDataStore defaultDataStore];
         [dataStore fetchDataRecordsOfTypes:websiteDataTypes
                          completionHandler:^(NSArray<WKWebsiteDataRecord *> * _Nonnull records) {
                              //NSLog(@"Record = %@", records);
-                             
+
                              for (WKWebsiteDataRecord *record  in records) {
                                  /* only delete records for the specified domain */
                                  if ( [record.displayName containsString:options[@"domain"]]) {
@@ -74,9 +74,9 @@ Calls are async operations and SHOULD BE CALLED FROM THE MAIN THREAD (?)
                              }
                          }
         ];
-        
+
         [self success];
-            
+
     }
     @catch (NSException *exception) {
         NSLog(@"%@", exception);
@@ -87,23 +87,21 @@ Calls are async operations and SHOULD BE CALLED FROM THE MAIN THREAD (?)
 
 - (void)success
 {
-    NSLog(@"%@", resultMsg);
-    
     CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-    
+
     [self.commandDelegate sendPluginResult:result callbackId:self.command.callbackId];
 }
-            
-            
+
+
 - (void)error:(NSString *)message
 {
     NSString *resultMsg = [NSString stringWithFormat:@"Error while clearing wkwebview cache (%@).", message];
     NSLog(@"%@", resultMsg);
-    
+
     CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
                                                 messageAsString:[resultMsg stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet] ]];
-    
+
     [self.commandDelegate sendPluginResult:result callbackId:self.command.callbackId];
 }
-            
+
 @end
